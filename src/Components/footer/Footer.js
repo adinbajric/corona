b46e23122cafe4d3d525d3d0c2285c87ff98e5fd
Adin Bajric <63010916+adinbajric@users.noreply.github.com>
2021-05-08 01:26:12 +0200

    hero and footer

--- src/Components/footer/Footer.js
+++ src/Components/footer/Footer.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import White from '../../assets/White.svg';
-import './Footer.css';
+import React from "react";
+import White from "../../assets/White.svg";
+import "./Footer.css";
 
 function Footer() {
-    return (
-        <footer>
-                <div className='footer-container'>
-                    <div className='footer-first'>
-                            <img src={White} className='logo' alt='logo'/>
-                            <p>Earth Heart Foundation Cebu © 2020. All rights reserved.</p>                        
-                    </div>
-                    <div className='footer-second'>
-                        <p>Privacy Policy</p>
-                        <p>Terms & Conditions</p>
-                        <div>
-                            <p>+63 911 111 1111</p>
-                            <p>Unit 12 Bldg Name, Street Name</p>
-                            <p>City, Country 4444</p>
-                        </div>
-                    </div>
-                </div>                
-            </footer>
-    )
+  return (
+    <footer>
+      <div className="footer-container">
+        <div className="footer-first">
+          <img src={White} className="logo" alt="logo" />
+          <p>Covid-19 © 2021. All rights reserved.</p>
+        </div>
+        <div className="footer-second">
+          <p>Privacy Policy</p>
+          <p>Terms & Conditions</p>
+          <div>
+            <p>+63 911 111 1111</p>
+            <p>Unit 12 Bldg Name, Street Name</p>
+            <p>City, Country 4444</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/Components/home/Hero.js
+++ src/Components/home/Hero.js
@@ -1,33 +1,68 @@
-import React,{useState} from 'react'
-import './Hero.css';
-import ic_ig from '../../assets/ic_ig.svg';
-import ic_yt from '../../assets/ic_yt.png';
-import Fill from '../../assets/Fill.svg';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Hero.css";
+import ic_ig from "../../assets/ic_ig.svg";
+import ic_yt from "../../assets/ic_yt.png";
+import Fill from "../../assets/Fill.svg";
 
 function Hero() {
-    const[hide, setHide] = useState(true);
+  const [hide, setHide] = useState(true);
 
-    return (
-        <div className='hero'>
-            <div className='hero__container'>
-                <h1 className='hero__heading'>We are fighting with <br /> Coronavirus!</h1>
-                <p className='hero__text'>Stay Aware of the Coronavirus, Stay Home & Safe For You and Others, And Maintain All the Safety Measures by Medical Professionals
-                to Defeat It!</p>
-                <button onClick={() => setHide(!hide)} className='btn btn__standard'>{hide ? 'Read more' : 'Show less'}</button>
-                <p className={hide ? 'notVisible' : 'hero__text'}>If COVID-19 is spreading in your community, stay safe by taking some simple precautions, such as physical distancing, wearing a mask, keeping rooms well ventilated, avoiding crowds, cleaning your hands, and coughing into a bent elbow or tissue. Check local advice where you live and work. Do it all!You also find out more about WHO's recommendations for getting vaccinated on our public advice page on COVID-19 vaccines. </p>
-            </div>
-            <div className='hero__footer'>
-                <p className='hero__number'>+1 797 735 3618</p>
-                <div className='hero__social'>
-                    <img className='hero__icon' src={ic_ig} alt='instagram'/>
-                    <img className='hero__icon' src={ic_yt} alt='youtube'/>
-                    <img className='hero__icon' src={Fill} alt='facebook'/>
-                </div>
-            </div>
+  return (
+    <div className="hero">
+      <div className="hero__container">
+        <h1 className="hero__heading">
+          We are fighting with <br /> Coronavirus!
+        </h1>
+        <p className="hero__text">
+          Stay Aware of the Coronavirus, Stay Home & Safe For You and Others,
+          And Maintain All the Safety Measures by Medical Professionals to
+          Defeat It!
+        </p>
+        <button onClick={() => setHide(!hide)} className="btn btn__standard">
+          {hide ? "Read more" : "Show less"}
+        </button>
+        <p className={hide ? "notVisible" : "hero__text"}>
+          If COVID-19 is spreading in your community, stay safe by taking some
+          simple precautions, such as physical distancing, wearing a mask,
+          keeping rooms well ventilated, avoiding crowds, cleaning your hands,
+          and coughing into a bent elbow or tissue. Check local advice where you
+          live and work. Do it all!You also find out more about WHO's
+          recommendations for getting vaccinated on our public advice page on
+          COVID-19 vaccines.{" "}
+        </p>
+      </div>
+      <div className="hero__footer">
+        <p className="hero__number">+1 797 735 3618</p>
+        <div className="hero__social">
+          <Link
+            to={{
+              pathname: "https://www.instagram.com/who/",
+            }}
+            target="_blank"
+          >
+            <img className="hero__icon" src={ic_ig} alt="instagram" />
+          </Link>
+          <Link
+            to={{
+              pathname: "https://www.youtube.com/user/who",
+            }}
+            target="_blank"
+          >
+            <img className="hero__icon" src={ic_yt} alt="youtube" />
+          </Link>
+          <Link
+            to={{
+              pathname: "https://www.facebook.com/WHO",
+            }}
+            target="_blank"
+          >
+            <img className="hero__icon" src={Fill} alt="facebook" />
+          </Link>
         </div>
-        
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
-
