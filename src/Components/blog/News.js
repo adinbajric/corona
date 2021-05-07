e142396aa4a988423b7841f5c47b2b8556e240a2
Adin Bajric <63010916+adinbajric@users.noreply.github.com>
2021-05-08 00:29:56 +0200

    third c

--- src/Components/blog/News.js
+++ src/Components/blog/News.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./News.css";
 import Article from "./Article";
 import Loading from "../loading/Loading";
--- src/Components/contacts/Form.js
+++ src/Components/contacts/Form.js
@@ -1,54 +1,50 @@
-import React,{useState} from 'react'
-import './Form.css';
+import React from "react";
+import "./Form.css";
 
-function Form({showPopUp}) {
-    /* const[name, setName] = useState('')
-    const[number, setNumber] = useState('');
-    const[email, setEmail] = useState('');
-    const[textArea, setTextArea] = useState(''); */
+function Form({ showPopUp }) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    showPopUp();
+  };
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        showPopUp()
-    }
-   
-    return (   
-           <form className='form__contact'>
-                 <label htmlFor='name'>
-                     <input
-                        type='text' 
-                        placeholder='Full name' 
-                        id='name'
-                        className='form__input'
-                        />
-                 </label>
-                 <label htmlFor='number'>
-                     <input
-                        type='number' 
-                        placeholder='Phone number' 
-                        id='number'
-                        className='form__input'
-                        />
-                 </label>
-                 <label htmlFor='email'>
-                     <input 
-                        type='email' 
-                        placeholder='E-mail adress' 
-                        id='email'
-                        className='form__input'
-                        />
-                 </label>
-                 <label htmlFor='message'>
-                     <textarea 
-                        placeholder='Message' 
-                        id='Message' 
-                        className='form__input contact__textarea'>                        
-                     </textarea>
-                 </label>
-                 <button onClick={handleSubmit} className='btn btn__form'>Send a message</button>
-             </form>   
-          
-    )
+  return (
+    <form className="form__contact">
+      <label htmlFor="name">
+        <input
+          type="text"
+          placeholder="Full name"
+          id="name"
+          className="form__input"
+        />
+      </label>
+      <label htmlFor="number">
+        <input
+          type="number"
+          placeholder="Phone number"
+          id="number"
+          className="form__input"
+        />
+      </label>
+      <label htmlFor="email">
+        <input
+          type="email"
+          placeholder="E-mail adress"
+          id="email"
+          className="form__input"
+        />
+      </label>
+      <label htmlFor="message">
+        <textarea
+          placeholder="Message"
+          id="Message"
+          className="form__input contact__textarea"
+        ></textarea>
+      </label>
+      <button onClick={handleSubmit} className="btn btn__form">
+        Send a message
+      </button>
+    </form>
+  );
 }
 
-export default Form
+export default Form;
